37ae3727d49b99e2fcca78ccc18ada9905dc7f27
Jordan <jordanmanu@live.com.au>
2022-07-04 16:29:31 +0100

    passing tests for leap year - number divisible by 4 and 400

---
+++ spec/leap_year_spec.rb
@@ -0,0 +1,16 @@
+require 'leap_year'
+
+describe 'leap year' do
+  it 'returns true or false' do
+    expect(leap_year(4)).to_not be_nil
+  end
+  it 'returns true if number is divisible by 4' do 
+    expect(leap_year(4)).to eq true
+  end
+  it 'returns false if number is not divisible by 4' do 
+    expect(leap_year(5)).to eq false
+  end
+  it 'returns true if number is divisible by 400' do 
+    expect(leap_year(1600)).to eq true
+  end
+end
